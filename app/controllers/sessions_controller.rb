580a5c0548590c88988c85bc2fbda6b4ac1700a1
Nick Playfair <nick@chaosemerald.co.uk>
2015-07-16 00:59:33 +0100

    Building login system

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log the user in and redirect
+      log_in user
+      redirect_to user # rails infers route to user url
+    else
+      # create an error message
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
