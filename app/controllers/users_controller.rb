14141acc5726fe78ca4ed7682615f6ad994fc413
Nick Playfair <nick@chaosemerald.co.uk>
2015-07-15 19:57:08 +0100

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        # handle a successful save
+        flash[:success] = "Welcome to the Sample App!"
+        # Rails infers redirect_to user_url(@user)
+        redirect_to @user
     else
       render 'new'
     end
