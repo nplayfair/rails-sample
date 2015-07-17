51de6323bb213c34cb5b3d7a9004be73eecf0677
Nick Playfair <nick@chaosemerald.co.uk>
2015-07-18 00:23:51 +0100

    Fixed login and associated methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # log the user in and redirect
       log_in user
+      remember user
       redirect_to user # rails infers route to user url
     else
       # create an error message
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
