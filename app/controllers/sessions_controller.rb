225d371f45469a5c8c5055149468df6aec2dfc71
Nick Playfair <nick@chaosemerald.co.uk>
2015-08-02 19:52:55 +0100

    Refactor current user check and better redirection based on where the user probably wants to go

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # log the user in and redirect
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user # rails infers route to user url
+      redirect_back_or user
     else
       # create an error message
       flash.now[:danger] = "Invalid email/password combination"
