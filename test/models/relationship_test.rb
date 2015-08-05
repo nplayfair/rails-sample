946ec1b253b078837563348ff5c32947e9c3cc6a
Nick Playfair <nick@chaosemerald.co.uk>
2015-08-05 12:26:08 +0100

    Add methods to follow, unfollow and check if followed by a user. Tests for those methods

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -19,4 +19,14 @@ class RelationshipTest < ActiveSupport::TestCase
     @relationship.followed_id = nil
     assert_not @relationship.valid?
   end
+
+  test "should follow and unfollow a user" do 
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
